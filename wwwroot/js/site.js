6fc24941d3a6a026b88e37fa57d9128a6773c6c2
Duy <dtvu@ncsu.edu>
2020-05-21 10:53:18 -0400

    added cost preview to add page, removed bin and obj folders

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -21,7 +21,7 @@ function getDependents(prefix) {
         el.LastName = document.getElementById(prefix + i + 'lname').value;
         el.IsSpouse = prefix === 'spouse';
 
-        if (el.FirstName && el.LastName) {
+        if (el.FirstName && el.LastName && dependents.indexOf(el) !== -1) {
             dependents.push(el);
         }
     }
@@ -44,7 +44,7 @@ function addNewRow(type) {
     var template = '<div class="row">' +
         '<div class="form-group">' +
         '<label for="' + id + count + 'fname" class="control-label">First Name</label>' +
-        '<input type="text" id="' + id + count + 'fname" class="form-control" />' +
+        '<input type="text" id="' + id + count + 'fname" class="form-control" onblur="setTotalCost()"/>' +
         '</div>' +
         '<div class="form-group">' +
         '<label for="' + id + count + 'lname" class="control-label">Last Name</label>' +
@@ -63,4 +63,55 @@ function addDependent(type) {
         div.innerHTML += addNewRow(type);
         dependentCounter.value++;
     }
+}
+
+function setTotalCost() {
+    var totalCosts = calculateEmployeeCost() + sumDependentCosts();
+    var totalCostsDiv = document.getElementById('totalCost');
+    totalCostsDiv.value = totalCosts;
+    totalCostsDiv.innerHTML = '$' + totalCosts.toFixed(2);
+}
+
+function calculateEmployeeCost() {
+    var firstName = document.getElementById('FirstName').value;
+    var employeeCostDiv = document.getElementById('employeeCost');
+
+    if (firstName.trim().toLowerCase().startsWith('a')) {
+        employeeCostDiv.value = 900;
+        employeeCostDiv.innerHTML = '$900.00';
+    } else {
+        employeeCostDiv.value = 1000;
+        employeeCostDiv.innerHTML = '$1,000.00';
+    }
+
+    return employeeCostDiv.value;
+}
+
+function sumDependentCosts() {
+    var cost = 0;
+
+    cost += getDependentCosts('spouse');
+    cost += getDependentCosts('child');
+
+    document.getElementById('dependentCost').value = cost;
+    document.getElementById('dependentCost').innerHTML = '$' + cost.toFixed(2);
+
+    return cost;
+}
+
+function getDependentCosts(type) {
+    var cost = 0;
+    var name = ''
+
+    for (i = 0; i < spouseCount; i++) {
+        var el = document.getElementById(type + i + 'fname');
+        name = el ? el.value : '';
+        if (name.trim().toLowerCase().startsWith('a')) {
+            cost += 450;
+        } else if (name){
+            cost += 500;
+        }
+    }
+
+    return cost;
 }
